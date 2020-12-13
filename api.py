b7098f9f261e75ab23a79e9d97d3765450741627
Conner Bondurant <conner.bondurant@triptych.me>
2020-12-13 00:44:48 -0500

    run yapf

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 import requests
 
+
 class API:
     def __init__(self, hostname):
         self.hostname = hostname + "/"
 
     def get(self, path, **kwargs):
-        return requests.get(self.hostname + path, headers={"User-Agent":"(WeatherBuddy, conner.bondurant@triptych.me)"}, **kwargs)
+        return requests.get(
+            self.hostname + path,
+            headers={
+                "User-Agent": "(WeatherBuddy, conner.bondurant@triptych.me)"
+            },
+            **kwargs)
--- weather.py
+++ weather.py
@@ -3,11 +3,13 @@ import requests
 import api
 import time
 
+
 class Weather:
     def __init__(self, **kwargs):
         self.api = api.API("https://api.weather.gov/")
         self.coordinate = kwargs["coordinates"]
-        station_data = json.loads(self.api.get("/points/{}".format(self.coordinate)).text)
+        station_data = json.loads(
+            self.api.get("/points/{}".format(self.coordinate)).text)
         self.gridId = station_data["properties"]["gridId"]
         self.gridX = station_data["properties"]["gridX"]
         self.gridY = station_data["properties"]["gridY"]
@@ -21,7 +23,8 @@ class Weather:
     def _validate_cache(self):
         curtime = time.time()
         if self.cacheTime - curtime < 0:
-            self.cache = self.api.get("/gridpoints/{}/{},{}/forecast".format(self.gridId, self.gridX, self.gridY)).json()
+            self.cache = self.api.get("/gridpoints/{}/{},{}/forecast".format(
+                self.gridId, self.gridX, self.gridY)).json()
             self.cacheTime = curtime + self.cacheTTL
 
     def get_forecast_raw(self):
@@ -31,9 +34,15 @@ class Weather:
     def get_weather_current(self):
         self._validate_cache()
         for forecast in self.cache["properties"]["periods"]:
-            start = time.strptime(forecast["startTime"],"%Y-%m-%dT%H:%M:%S%z")
-            end = time.strptime(forecast["endTime"],"%Y-%m-%dT%H:%M:%S%z")
+            start = time.strptime(forecast["startTime"], "%Y-%m-%dT%H:%M:%S%z")
+            end = time.strptime(forecast["endTime"], "%Y-%m-%dT%H:%M:%S%z")
             if start < time.gmtime() < end:
                 return forecast
 
         return self.cache["properties"]["weather"]["values"][0]["value"][0]
+
+    def is_raining():
+        hourly = self.api.get("/gridpoints/{}/{},{}".format(
+            self.gridId, self.gridX, self.gridY)).json()
+        return "rain_showers" == hourly['properties']['weather']['values'][0][
+            'value'][0]['weather']
